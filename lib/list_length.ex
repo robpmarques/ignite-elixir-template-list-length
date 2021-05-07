703edd4231e42614b918eb4d3afc45ed6f135104
Roberto Marques <matsufrg@gmail.com>
2021-05-07 18:42:44 -0300

    first commit

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    check_length(list, 0)
+  end
+
+  def check_length([], acc), do: acc
+
+  def check_length([_ | tail], acc) do
+    acc = acc + 1
+    check_length(tail, acc)
   end
 end
